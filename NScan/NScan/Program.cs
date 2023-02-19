ca05819545133734a8bc0db1dd9a5ab34f56e76e
Jonathan Ryrvall <>
2023-02-19 19:42:26 +0100

    - Lite tester

--- NScan/NScan/Program.cs
+++ NScan/NScan/Program.cs
@@ -10,9 +10,34 @@ public class Program
 
     static IServiceProvider Services;
 
-
+    static void PrintPortLine(int no, string ip, int port, string service)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.Write($"[{no}]");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"\t{ip}\t{port}\t{service}");
+     
+    }
     static void Main(string[] args)
     {
+        Console.WriteLine("No\tIP\t\tPort\tService");
+        Console.WriteLine("-----------------------------------------------");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+      //  Console.WriteLine("[192.168.1.2]");
+        PrintPortLine(0, "192.168.1.2", 25, "SMTP");
+        PrintPortLine(1, "192.168.1.2", 80, "HTTP");
+        PrintPortLine(2, "192.168.1.2", 445, "SMB");
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+   //     Console.WriteLine("[192.168.1.6]");
+        PrintPortLine(0, "192.168.1.6", 80, "HTTP");
+        PrintPortLine(1, "192.168.1.6", 445, "HTTPS");
+        PrintPortLine(2, "192.168.1.6", 1433, "SQL");
+        PrintPortLine(3, "192.168.1.6", 1677, "<Unknown>");
+        PrintPortLine(4, "192.168.1.6", 1883, "MQTT");
+
+        return;
         Services = ConfigureServices();
 
 
--- NScan/NScan/Services/DeviceService.cs
+++ NScan/NScan/Services/DeviceService.cs
@@ -10,35 +10,35 @@ namespace NScan.Services
 {
     internal class DeviceService
     {
-        internal List<Device> LoadDevices()
-        {
-            var devices = CaptureDeviceList.Instance;
+        //internal List<Device> LoadDevices()
+        //{
+        //    var devices = CaptureDeviceList.Instance;
 
-            foreach (var dev in devices)
-            {
-                Console.WriteLine("{0}\n", dev.ToString());
+        //    foreach (var dev in devices)
+        //    {
+        //        Console.WriteLine("{0}\n", dev.ToString());
 
-            }
-        }
+        //    }
+        //}
 
-        private Device FromPcapDevice(ILiveDevice device)
-        {
-            return new Device()
-            {
-                Name = GetFriendlyName(device),
-                PcapDevice = device,
-            };
-        }
+        //private Device FromPcapDevice(ILiveDevice device)
+        //{
+        //    return new Device()
+        //    {
+        //        Name = GetFriendlyName(device),
+        //        PcapDevice = device,
+        //    };
+        //}
 
-        private string GetFriendlyName(ILiveDevice device)
-        {
-            var lines = device.ToString().Split(new []
-            {
-                "\r\n", "\n"
-            }, StringSplitOptions.RemoveEmptyEntries);
-            var friendlyNameLine = lines.FirstOrDefault(l => l.Contains("Friendy"));
+        //private string GetFriendlyName(ILiveDevice device)
+        //{
+        //    var lines = device.ToString().Split(new []
+        //    {
+        //        "\r\n", "\n"
+        //    }, StringSplitOptions.RemoveEmptyEntries);
+        //    var friendlyNameLine = lines.FirstOrDefault(l => l.Contains("Friendy"));
 
-        }
+        //}
 
     }
 }
