4316db36bdbbd7089fc85ecd91d64d857f9a3e1b
Skroint <46086303+Skroint@users.noreply.github.com>
2023-03-01 19:44:21 +0100

    - Begin work on scan result

---
+++ NBTSCAN/found_host.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ipv4_address.h"
+#include "mac_address.h"
+
+class found_host
+{
+	ipv4_address ip;
+	mac_address mac;
+};
+
---
+++ NBTSCAN/scan_result.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "found_host.h"
+
+class scan_result
+{
+public:
+	found_host* found;
+};
+
