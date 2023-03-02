514785f7ca68fa3269f4bdaa841ddb1e7f168e3d
Skroint <46086303+Skroint@users.noreply.github.com>
2023-03-02 20:06:33 +0100

    - Some npcap stuff

--- NBTSCAN/arp_host_scanner.cpp
+++ NBTSCAN/arp_host_scanner.cpp
@@ -1,10 +1,30 @@
 #include "arp_host_scanner.h"
+#include <pcap.h>
 
 std::vector<std::string> arp_host_scanner::scan(std::vector<std::string> targets)
 {
+	
+
     return std::vector<std::string>();
 }
 
 void arp_host_scanner::scan(scan_result* sr, const std::vector<ipv4_address>& addresses)
 {
+	pcap_if_t* devices;
+
+	char error_buffer[PCAP_ERRBUF_SIZE + 1];
+
+	if (pcap_findalldevs(&devices, error_buffer) == -1)
+	{
+		fprintf(stderr, "Error in pcap_findalldevs: %s\n", error_buffer);
+	}
+
+	for (pcap_if_t* d = devices; d; d = d->next)
+	{
+
+	}
+
+
+
+
 }
